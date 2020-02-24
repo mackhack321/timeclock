84f960cb978a54412439215ec324325faa1bce59
Mack Stanley <mlgprohacker@gmail.com>
2020-02-24 11:54:59 -0600

    build employee creation dialog

---
+++ NewEmployee.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Timeclock
+{
+    partial class NewEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewEmployee));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxFName = new System.Windows.Forms.TextBox();
+            this.textBoxLName = new System.Windows.Forms.TextBox();
+            this.textBoxEmployeeID = new System.Windows.Forms.TextBox();
+            this.labelFName = new System.Windows.Forms.Label();
+            this.labelLName = new System.Windows.Forms.Label();
+            this.labelEmployeeID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxFName
+            // 
+            this.textBoxFName.Location = new System.Drawing.Point(75, 24);
+            this.textBoxFName.Name = "textBoxFName";
+            this.textBoxFName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFName.TabIndex = 1;
+            // 
+            // textBoxLName
+            // 
+            this.textBoxLName.Location = new System.Drawing.Point(75, 76);
+            this.textBoxLName.Name = "textBoxLName";
+            this.textBoxLName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLName.TabIndex = 2;
+            // 
+            // textBoxEmployeeID
+            // 
+            this.textBoxEmployeeID.Location = new System.Drawing.Point(75, 130);
+            this.textBoxEmployeeID.Name = "textBoxEmployeeID";
+            this.textBoxEmployeeID.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEmployeeID.TabIndex = 3;
+            // 
+            // labelFName
+            // 
+            this.labelFName.AutoSize = true;
+            this.labelFName.Location = new System.Drawing.Point(12, 27);
+            this.labelFName.Name = "labelFName";
+            this.labelFName.Size = new System.Drawing.Size(57, 13);
+            this.labelFName.TabIndex = 4;
+            this.labelFName.Text = "First Name";
+            // 
+            // labelLName
+            // 
+            this.labelLName.AutoSize = true;
+            this.labelLName.Location = new System.Drawing.Point(12, 79);
+            this.labelLName.Name = "labelLName";
+            this.labelLName.Size = new System.Drawing.Size(58, 13);
+            this.labelLName.TabIndex = 4;
+            this.labelLName.Text = "Last Name";
+            // 
+            // labelEmployeeID
+            // 
+            this.labelEmployeeID.AutoSize = true;
+            this.labelEmployeeID.Location = new System.Drawing.Point(3, 133);
+            this.labelEmployeeID.Name = "labelEmployeeID";
+            this.labelEmployeeID.Size = new System.Drawing.Size(67, 13);
+            this.labelEmployeeID.TabIndex = 4;
+            this.labelEmployeeID.Text = "Employee ID";
+            // 
+            // NewEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 241);
+            this.Controls.Add(this.labelEmployeeID);
+            this.Controls.Add(this.labelLName);
+            this.Controls.Add(this.labelFName);
+            this.Controls.Add(this.textBoxEmployeeID);
+            this.Controls.Add(this.textBoxLName);
+            this.Controls.Add(this.textBoxFName);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NewEmployee";
+            this.Text = "Employee Creation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxFName;
+        private System.Windows.Forms.TextBox textBoxLName;
+        private System.Windows.Forms.TextBox textBoxEmployeeID;
+        private System.Windows.Forms.Label labelFName;
+        private System.Windows.Forms.Label labelLName;
+        private System.Windows.Forms.Label labelEmployeeID;
+    }
+}
